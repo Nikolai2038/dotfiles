67da17f695f1c2e59ddad78e91cdc255e7629421
Nikolai Ivanov <nikolai.ivanov.2038@gmail.com>
2025-06-08 10:16:07 +0300

    New method to show/hide waybar
    Pluses:
    + More consistent (tray icons starts correctly);
    
    Minuses:
    - Because the waybar is still running in background, it will still process all the items, causing power consumption.

--- waybar/.config/waybar/toggle_waybar.sh
+++ waybar/.config/waybar/toggle_waybar.sh
@@ -1,14 +1,44 @@
 #!/bin/bash
 
 function main() {
-  if pgrep --exact --full waybar > /dev/null; then
-    echo "Hiding waybar..." >&2
-    pkill --exact --full waybar || return "$?"
-    echo "Hiding waybar: success!" >&2
+  echo "Applying 01_general..." >&2
+
+  cat "${HOME}/.config/waybar/01_general.css" > "${HOME}/.config/waybar/style.css" || return "$?"
+
+  # shellcheck disable=SC2320
+  echo -en "\n\n" >> "${HOME}/.config/waybar/style.css" || return "$?"
+
+  echo "Applying 01_general: success!" >&2
+
+  local state_file="${HOME}/.config/waybar/.waybar_is_hidden.lock"
+
+  if [ -f "${state_file}" ]; then
+    echo "Applying 02_shown..." >&2
+    rm "${state_file}" || return "$?"
+    cat "${HOME}/.config/waybar/02_shown.css" >> "${HOME}/.config/waybar/style.css" || return "$?"
+    echo "Applying 02_shown: success!" >&2
   else
-    echo "Showing waybar..." >&2
+    echo "Applying 02_hidden..." >&2
+    touch "${state_file}" || return "$?"
+    cat "${HOME}/.config/waybar/02_hidden.css" >> "${HOME}/.config/waybar/style.css" || return "$?"
+    echo "Applying 02_hidden: success!" >&2
+  fi
+
+  # If waybar is not started yet
+  if ! pgrep --exact --full waybar > /dev/null; then
+    echo "Starting waybar..." >&2
+
+    # Start waybar
     waybar &
-    echo "Showing waybar: success!" >&2
+
+    echo "Starting waybar: success!" >&2
+  else
+    echo "Reloading waybar..." >&2
+
+    # Reload waybar
+    pkill -SIGUSR2 waybar
+
+    echo "Reloading waybar: success!" >&2
   fi
 }
 
