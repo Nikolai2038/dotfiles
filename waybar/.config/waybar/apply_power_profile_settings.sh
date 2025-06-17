3877f4023cd2724e9e1a8a2e725adcae9da8b248
Nikolai Ivanov <nikolai.ivanov.2038@gmail.com>
2025-06-17 10:39:37 +0300

    Optimize applying power profile settings for Hyprland

--- waybar/.config/waybar/apply_power_profile_settings.sh
+++ waybar/.config/waybar/apply_power_profile_settings.sh
@@ -4,22 +4,11 @@ function main() {
   local profile
   profile="$(powerprofilesctl get)" || return "$?"
 
-  if [ "${profile}" = "power-saver" ]; then
-    brightnessctl set 25% || return "$?"
-    hyprctl dispatch exec "hyprctl keyword monitor 'DP-1, 1920x1080@60, 0x0, 1'" || return "$?"
-    hyprctl dispatch exec "hyprctl keyword monitor 'DP-2, 1920x1080@60, 1920x0, 1'" || return "$?"
-    hyprctl dispatch exec "hyprctl keyword monitor 'eDP-2, 1920x1080@60, 3840x0, 1'" || return "$?"
-  elif [ "${profile}" = "balanced" ]; then
-    brightnessctl set 50% || return "$?"
-    hyprctl dispatch exec "hyprctl keyword monitor 'DP-1, 1920x1080@60, 0x0, 1'" || return "$?"
-    hyprctl dispatch exec "hyprctl keyword monitor 'DP-2, 2560x1440@60, 1920x-360, 1'" || return "$?"
-    hyprctl dispatch exec "hyprctl keyword monitor 'eDP-2, 2560x1440@60, 4480x0, 1.6'" || return "$?"
-  elif [ "${profile}" = "performance" ]; then
-    brightnessctl set 100% || return "$?"
-    hyprctl dispatch exec "hyprctl keyword monitor 'DP-1, 1920x1080@144, 0x0, 1'" || return "$?"
-    hyprctl dispatch exec "hyprctl keyword monitor 'DP-2, 2560x1440@175, 1920x-360, 1'" || return "$?"
-    hyprctl dispatch exec "hyprctl keyword monitor 'eDP-2, 2560x1440@165, 4480x0, 1.6'" || return "$?"
-  fi
+  # Update config file
+  cp "${HOME}/.config/hypr/hyprland.conf.d/.05_power_${profile}.conf" ~/.config/hypr/hyprland.conf.d/05_power.conf || return "$?"
+
+  # Reload Hyprland
+  hyprctl reload || return "$?"
 }
 
 main "$@" || exit "$?"
