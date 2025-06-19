3e24317c82e79c5574056c6f49c62215a5d1e188
Nikolai Ivanov <nikolai.ivanov.2038@gmail.com>
2025-06-19 09:19:59 +0300

    Open and toggle terminal in NeoVim

--- neovim/.config/nvim/init.lua
+++ neovim/.config/nvim/init.lua
@@ -81,3 +81,57 @@ vim.keymap.set("n", "<A-a>", "<C-w>h")  -- focus left window
 vim.keymap.set("n", "<A-w>", "<C-w>k")  -- focus upper window
 vim.keymap.set("n", "<A-s>", "<C-w>j")  -- focus lower window
 vim.keymap.set("n", "<A-d>", "<C-w>l")  -- focus right window
+
+-- ==============================
+-- Terminal
+-- ==============================
+-- Variables to keep track of terminal window and buffer
+local term_win = nil
+local term_buf = nil
+local prev_win = nil
+
+-- Autocmd to change Alt+3 in terminal mode
+vim.api.nvim_create_autocmd("TermOpen", {
+  callback = function()
+    vim.keymap.set("t", "<A-3>", function()
+      vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<C-\\><C-n>", true, false, true), "n", false)
+      -- Replace function on Alt+3
+      vim.defer_fn(function()
+        vim.api.nvim_win_hide(term_win)
+        term_win = nil
+
+	-- Focus on the previous window
+        if prev_win and vim.api.nvim_win_is_valid(prev_win) then
+          vim.api.nvim_set_current_win(prev_win)
+        end
+      end, 10)
+    end, { buffer = true, noremap = true, silent = true })
+  end,
+})
+
+-- Keymap to toggle the Terminal on Alt+3 in normal mode
+-- TODO: Exit NeoVim if only the Terminal and/or File Browser is opened
+vim.keymap.set("n", "<A-3>", function()
+  prev_win = vim.api.nvim_get_current_win()
+  
+  if term_buf and vim.api.nvim_buf_is_valid(term_buf) then
+    -- Terminal buffer exists, open it in a split
+    vim.cmd("botright split")
+    vim.cmd("resize 16")
+    local win = vim.api.nvim_get_current_win()
+    vim.api.nvim_win_set_buf(win, term_buf)
+    term_win = win
+  else
+    -- No terminal buffer, create new terminal in split
+    vim.cmd("botright split")
+    vim.cmd("resize 16")
+    vim.cmd("term")
+    term_buf = vim.api.nvim_get_current_buf()
+    term_win = vim.api.nvim_get_current_win()
+  end
+  -- Focus the terminal window and enter insert mode
+  vim.api.nvim_set_current_win(term_win)
+  vim.cmd("startinsert")
+end, { noremap = true, silent = true })
+-- ==============================
+
