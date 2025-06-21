405c17cab7af5e71d0396bcd57b93bc766631b12
Nikolai Ivanov <nikolai.ivanov.2038@gmail.com>
2025-06-21 15:49:55 +0300

    Use smart gaps

--- waybar/.config/waybar/toggle_waybar.sh
+++ waybar/.config/waybar/toggle_waybar.sh
@@ -5,10 +5,16 @@ function main() {
     echo "Hiding waybar..." >&2
     pkill --exact --full waybar || return "$?"
     echo "Hiding waybar: success!" >&2
+
+    # Because waybar is responsible for bottom gap, we recalculate it here
+    hyprctl dispatch exec "hyprctl keyword general:gaps_out 4,4,4,4"
   else
     echo "Showing waybar..." >&2
     waybar &
     echo "Showing waybar: success!" >&2
+
+    # Because waybar is responsible for bottom gap, we recalculate it here
+    hyprctl dispatch exec "hyprctl keyword general:gaps_out 4,4,0,4"
   fi
 }
 
