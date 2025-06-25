d48c03144ad3999cf36f62be5571ab512d44a759
Nikolai Ivanov <nikolai.ivanov.2038@gmail.com>
2025-06-26 02:13:05 +0300

    Open new windows on current focused workspace, not on the default one

--- hyprland/.config/hypr/focus_or_open_app.sh
+++ hyprland/.config/hypr/focus_or_open_app.sh
@@ -1,14 +1,28 @@
 #!/bin/bash
 
 function main() {
-  local class_name="${1}" && shift
+  local class="${1}" && shift
+  local default_workspace="${1}" && shift
+  local is_new_window="${1:-0}" && shift
 
-  if [ "$(hyprctl dispatch focuswindow "class:^(${class_name})\$")" = "No such window found" ]; then
-    # # This for some reason not moving focus, when executing this script in "bind" (or as "hyprctl dispatch exec ~/.config/hypr/focus_or_open_app.sh").
-    # # So instead, I use "windowrule" for apps.
-    # hyprctl dispatch workspace 101 || return "$?"
+  if [ "${is_new_window}" = "0" ]; then
+    if [ "$(hyprctl dispatch focuswindow "class:^(${class})\$")" = "No such window found" ]; then
+      # Switching workspace before opening the app is not enough, because the app will be opened on the workspace with current keyboard focus.
+      # And the keyboard focus will not change when workspace switches via this command.
+      # hyprctl dispatch workspace "${default_workspace}" || return "$?"
 
-    uwsm app -- "${class_name}.desktop" || return "$?"
+      # Using workspace rule in the Hyprland config itself is bad, because it will force new windows to open on the default workspace too.
+      # Instead, we create this rule dynamically, so it only applies until "hyprctl reload" is called.
+      # - Wiki about "keyword": https://wiki.hypr.land/Configuring/Using-hyprctl/#keyword
+      hyprctl keyword windowrulev2 "workspace ${default_workspace},class:^(${class})\$" || return "$?"
+
+      uwsm app -- "${class}.desktop" || return "$?"
+    fi
+  elif [ "${is_new_window}" = "1" ]; then
+    # Reload Hyprland rules to not include the "default_workspace" rule above.
+    hyprctl reload || return "$?"
+
+    uwsm app -- "${class}.desktop" || return "$?"
   fi
 }
 
